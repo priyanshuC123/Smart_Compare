2c30c2e0caadc3443fcdc0779bbcf47dea1fd1d3
Priyanshu Choudhary <priyanschouhary100@gmail.com>
2024-07-12 16:05:16 +0530

    remove something

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://nodeserver-priyanshuc123-priyanshuc123s-projects.vercel.app/',
+  baseURL: 'https://nodeserver-priyanshuc123-priyanshuc123s-projects.vercel.app',
   headers: {
     'Content-Type': 'application/json',
   },
